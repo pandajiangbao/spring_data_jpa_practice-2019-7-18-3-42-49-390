58fc5d9be3c2c635555c4fc15a857c70a4d29f34
WUPA2 <panda.wu@oocl.com>
2019-07-18 16:52:53 +0800

    finish story 2

---
+++ src/main/resources/db/migration/V2__update_prosecutor_structure.sql
@@ -0,0 +1,7 @@
+ALTER TABLE `Prosecutor` ADD `age` int;
+ALTER TABLE `Prosecutor` ADD `sex` varchar(10);
+ALTER TABLE `Prosecutor` ADD `birthday` varchar(20);
+ALTER TABLE `Prosecutor` ADD `ID-number` varchar(30);
+ALTER TABLE `Prosecutor` ADD `experience` varchar(255);
+
+
